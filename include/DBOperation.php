0203c873e80ec1cfd6d897277c2a9dddaf03968a
Sid Verma <mavidser@gmail.com>
2017-07-23 03:33:14 +0530

    fuck shit up fam

--- include/DBOperation.php
+++ include/DBOperation.php
@@ -23,62 +23,86 @@ class DBOperation {
     }
 
     public function sendLocation($data) {
-
-        //Save data in the users table come from pinging
-        $query = $this->con->prepare("INSERT INTO users(username, location, created_at) values(?, ?, ?)");
-        $query->bind_param($data['username'], json_encode($data['location']));
+        $query = $this->con->prepare("INSERT INTO location_details(user_id, location, time) values(?, ?, ?)");
+        $currentTime=time();
+        $query->bind_param('ssd', $data['user_id'], $data['location'], $currentTime);
         $result = $query->execute();
         $query->close();
-
         if($result) {
-
             return 0;
-
         } else {
-
             return 1;
-
         }
 
     }
 
     public function getLocation($data) {
 
-        //Send data from users table
-        $query = $this->con->prepare("SELECT * FROM `location_details` WHERE id = ?");
-        $query->bind_param('s', $data['id']);
+        //Send data from location_details table
+        $query = $this->con->prepare("SELECT * FROM `location_requests` WHERE id = ?");
+        $query->bind_param('d', $data['id']);
         $query->execute();
         //Getting the student result array
         $result = $query->get_result()->fetch_assoc();
         $query->close();
-
-        if($result['permit_status'] == 'granted') {
-            if(strtotime($result['created_at']) > strtotime('-5 minutes')) {
-                $statement = $this->con->prepare("SELECT * FROM `users` WHERE (id = ? AND created_at > DATE_ADD(NOW(), INTERVAL -6 HOUR))");
-                $statement->bind_param('s', $result['user_id']);
+        if($result['permit_status'] == 'rejected') {
+            return 'safe';
+        } else {
+            if($result['permit_status'] == 'granted' || $result['time'] < strtotime('-5 minutes')) {
+                $statement = $this->con->prepare("SELECT `location`, `time` FROM `location_details` WHERE (user_id = ? AND time > ?)");
+                $time_lower = strtotime('-6 hours');
+                $statement->bind_param('sd', $result['user_id'], $time_lower);
                 $statement->execute();
                 //Getting the student result array
-                $information = $statement->get_result()->fetch_assoc();
+                $information = $statement->get_result();
+                $result_object = array();
+                while ( $row = $information->fetch_assoc()) {
+                    array_push($result_object, $row);
+                }
                 $statement->close();
-                print_r($information);die;
+                return $result_object;
+            } else {
+                return 'wait';
             }
-//            return $result;
-        } else {
-            print_r('Fuck off!!');die;
-//            return $result;
         }
-        //returning the student
-//        return $result;
-
-    }
-
-    public function checkPermit($data) {
-        //TODO: Implementation of backend
-        print_r($data);die;
     }
 
     public function fetchLocationHistory($data) {
-        //TODO: Implementation of backend
-        print_r($data);die;
+        $query = $this->con->prepare("INSERT INTO location_requests(asker_id, user_id, time) values(?, ?, ?)");
+        $currentTime=time();
+        $query->bind_param('ssd', $data['asker_id'], $data['user_id'], $currentTime);
+        $result = $query->execute();
+        $query->close();
+        if($result) {
+            return $this->con->insert_id;
+        } else {
+            return False;
+        }
     }
+
+    public function registerGcm($data) {
+        $query = $this->con->prepare("INSERT INTO user_gcm_details(user_id, gcm) values(?, ?) ON DUPLICATE KEY UPDATE gcm = ?;");
+        $query->bind_param('sss', $data['user_id'], $data['gcm'], $data['gcm']);
+        $result = $query->execute();
+        $query->close();
+        if($result) {
+            return True;
+        } else {
+            return False;
+        }
+    }
+
+    public function checkPermit($data) {
+        $query = $this->con->prepare("UPDATE location_requests SET permit_status = ? where id = ?");
+        $query->bind_param('ss', $data['permit'], $data['id']);
+        $result = $query->execute();
+        $query->close();
+        if($result) {
+            return True;
+        } else {
+            return False;
+        }
+    }
+
+
 }
--- v1/index.php
+++ v1/index.php
@@ -9,114 +9,106 @@
 require_once '../include/DbOperation.php';
 require '../vendor/autoload.php';
 use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Message\ResponseInterface;
 
 $app = new Slim\App;
 
-function echoResponse($status_code, $response) {
-
-    global $app;
-
-    //Setting Http response code
-    $app->status($status_code);
-
-    //setting response content type to json
-    $app->contentType('application/json');
-
-    //displaying the response in json format
-    echo json_encode($response);
+function echoResponse($value, $response) {
+    return $response->withJson($value);
 }
 
-$app->post('/sendLocation', function(ServerRequestInterface $request) {
+$app->post('/sendLocation', function(ServerRequestInterface $request, ResponseInterface $response_object) {
 
     $response = [];
 
     $db = new DBOperation();
-    $data =  $request->getQueryParams();
+    $data =  $request->getParsedBody();
     $result = $db->sendLocation($data);
 
     if($result == 0) {
         $response['success'] = true;
         $response['message'] = 'Location Send';
-        echoResponse(200, $response);
+        return echoResponse($response, $response_object);
     } elseif ($result == 1 ) {
         $response['success'] = false;
         $response['message'] = 'Oops! Error occured';
-        echoResponse(200, $response);
+        return echoResponse($response, $response_object);
     } else {
         $response['success'] = false;
         $response['message'] = 'Something went wrong !';
-        echoResponse(200, $response);
+        return echoResponse($response, $response_object);
     }
 });
 
-$app->get('/getLocation', function(ServerRequestInterface $request) use ($app) {
+$app->get('/getLocation', function(ServerRequestInterface $request, ResponseInterface $response_object) use ($app) {
 
-//    $response = [];
+    $response = [];
 
     $db = new DBOperation();
     $data =  $request->getQueryParams();
     $result = $db->getLocation($data);
-
-    if($result == 0) {
-        $response['success'] = true;
-        $response['message'] = 'Got Location';
-        echoResponse(200, $response);
-    } elseif ($result == 1) {
-        $response['success'] = false;
-        $response['message'] = 'Oops ! Error occured';
-        echoResponse(200, $response);
-    } else {
-        $response['success'] = false;
-        $response['message'] = 'Something went wrong !';
-        echoResponse(200, $response);
-    }
+    $response['success'] = true;
+    $response['message'] = $result;
+    return echoResponse($response, $response_object);
 
 });
 
-$app->post('/locationPermit', function(ServerRequestInterface $request) use ($app) {
+$app->post('/locationPermit', function(ServerRequestInterface $request, ResponseInterface $response_object) use ($app) {
 
     $response = [];
 
     $db = new DBOperation();
-    $data =  $request->getQueryParams();
-    $result = $db->checkpermit($data);
+    $data =  $request->getParsedBody();
+    $result = $db->checkPermit($data);
 
-    if($result == 0) {
-        $response['success'] = true;
-        $response['message'] = 'Location History';
-        echoResponse(200, $response);
-    } elseif ($result == 1) {
-        $response['success'] = false;
-        $response['message'] = 'Oops ! Error occured';
-        echoResponse(200, $response);
+    if($result == False) {
+        $response['success'] = False;
+        $response['message'] = 'Failed to set permission';
+        return echoResponse($response, $response_object);
     } else {
-        $response['success'] = false;
-        $response['message'] = 'Something went wrong !';
-        echoResponse(200, $response);
+        $response['success'] = true;
+        $response['message'] = 'Saved permission';
+        return echoResponse($response, $response_object);
     }
 
 });
 
-$app->post('/locationHistory', function(ServerRequestInterface $request) use ($app) {
+$app->post('/fetchLocationHistory', function(ServerRequestInterface $request, ResponseInterface $response_object) use ($app) {
 
     $response = [];
 
     $db = new DBOperation();
-    $data =  $request->getQueryParams();
+    $data =  $request->getParsedBody();
     $result = $db->fetchLocationHistory($data);
 
-    if($result == 0) {
-        $response['success'] = true;
-        $response['message'] = 'Location History';
-        echoResponse(200, $response);
-    } elseif ($result == 1) {
-        $response['success'] = false;
-        $response['message'] = 'Oops ! Error occured';
-        echoResponse(200, $response);
+    if($result == False) {
+        $response['success'] = False;
+        $response['message'] = $result;
+        return echoResponse($response, $response_object);
     } else {
-        $response['success'] = false;
-        $response['message'] = 'Something went wrong !';
-        echoResponse(200, $response);
+        $response['success'] = true;
+        $response['message'] = $result;
+        return echoResponse($response, $response_object);
+    }
+
+});
+
+$app->post('/registerGcm', function(ServerRequestInterface $request, ResponseInterface $response_object) use ($app) {
+
+    $response = [];
+
+    $db = new DBOperation();
+    $data =  $request->getParsedBody();
+    $result = $db->registerGcm($data);
+
+    if($result == False) {
+        $response['success'] = False;
+        $response['message'] = 'Failed to register GCM';
+        return echoResponse($response, $response_object);
+    } else {
+        $response['success'] = true;
+        $response['message'] = 'Registered GCM';
+        return echoResponse($response, $response_object);
     }
 
 });
